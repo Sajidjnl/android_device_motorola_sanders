59591316feed4bed57a2eb219735719c83587f3d
Syed Sajid <syedsajid341@gmail.com>
2018-08-12 11:03:58 +0000

    fix typo

--- mdroid.mk
+++ mdroid.mk
@@ -19,7 +19,7 @@
 $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 
 # Inherit some common CosmicOS stuff.
-$(call inherit-product, vendor/mdroid/common._full_phonemk)
+$(call inherit-product, vendor/mdroid/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
